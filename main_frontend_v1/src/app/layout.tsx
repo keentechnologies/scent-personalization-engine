f5863670568a63364b330cfbd0a0a8a69904bdf4
Nilansh <nilansh.nk@gmail.com>
2026-07-14 15:36:08 +0530

    google tag script add

--- main_frontend_v1/src/app/layout.tsx
+++ main_frontend_v1/src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Footer } from "@/components/layout/footer";
 import { ComingSoonProvider } from "@/components/coming-soon/coming-soon-provider";
 import { ClarityProvider } from "@/components/clarity-provider/clarity-provider";
 
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://craftedsprays.com"),
@@ -29,6 +31,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${cormorant.variable} ${inter.variable}`}>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17772674522"></Script>
+        <Script id="google-tag" strategy= "afterInteractive" >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17772674522');
+          `}
+        </Script>
+      </head>
       <body className="grain-overlay relative min-h-screen bg-bg text-text-primary">
         <ClarityProvider />
         <ComingSoonProvider>
